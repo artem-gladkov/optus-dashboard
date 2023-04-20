d1ec49e01d7d713c07810f043ff41cc1bd9e00d7
AlexeyNekrasov <necrasoff.alex@yandex.ru>
2023-04-20 14:03:37 +0000

    fix background

--- src/components/background/bg.tsx
+++ src/components/background/bg.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import useMedia from "../../hooks/useMedia"
 import "./bg.css"
 
 type Props = {
@@ -6,10 +7,14 @@ type Props = {
 }
 
 const Bg = (props: Props) => {
+
+
+
   return (
-    <div className='swapanimation  relative z-10  bg-bg h-full w-full'>
+    <div className='swapanimation   z-10  bg-bg h-full w-full'>
       <div className='swapbutton h-full w-full z-10 flex justify-center bg-bg '>
           <span className=''></span>
+          <span className='-ml-80'></span>
           <span className='-ml-80'></span>
           <span className='-ml-80'></span>
           <span className='-ml-80'></span>
