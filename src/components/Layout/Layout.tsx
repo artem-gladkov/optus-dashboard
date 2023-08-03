1a570fe961624199472c26b348528cee6d343bb0
Alexey Nekrasov <necrasoff.alex@yandex.ru>
2023-08-03 15:38:37 +0000

    add: routh path in layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,50 +1,58 @@
-import { Header } from '../header/Header'
-import { Outlet, useParams} from "react-router-dom"
-import { observer } from "mobx-react-lite"
-import { store } from '../../Store/store';
-import { useEffect } from 'react';
-import { RigthNavigations } from '../right-navigations/rigth-navigations';
-import Bg from '../background/bg';
+import { Header } from "../header/Header";
+import { Outlet, useParams } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+import { store } from "../../Store/store";
+import { useEffect } from "react";
+import { RigthNavigations } from "../right-navigations/rigth-navigations";
+import Bg from "../background/bg";
 
 interface Props {}
 
 const LayoutComponent = (props: Props) => {
+  const { dex } = useParams();
+  const { updateActiveButtonDex, getActiveButtonDex } = store;
+  const pathWindow = window.location.pathname;
+  const pairs = "pairs";
+  const tokens = "tokens";
+  const path = pathWindow === "/";
+  const pathPage = pathWindow.includes("dexoverview")
+    ? {
+        toCoins: `/dexoverview/${tokens}`,
+        toPairs: `/dexoverview/${pairs}`,
+      }
+    : {
+        toCoins: `/overview/${dex ? dex : getActiveButtonDex}/${tokens}`,
+        toPairs: `/overview/${dex ? dex : getActiveButtonDex}/${pairs}`,
+      };
 
-const { updateActiveButtonDex, getActiveButtonDex } = store
+  useEffect(() => {
+    updateActiveButtonDex(window.location.pathname.split("/")[2]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-const pairs = "pairs";
-const tokens = "tokens";
-const path = window.location.pathname === '/'
-const { dex } = useParams();
-
-useEffect(() => {
-  updateActiveButtonDex(window.location.pathname.split('/')[2])
-// eslint-disable-next-line react-hooks/exhaustive-deps
-}, [])
-
+  console.log(window.location.pathname);
   return (
-    <div className='flex flex-col w-full h-full justify-start text-text'>
-      
-        <div className='h-22 w-full z-50'>
-          <Header/>
-        </div>
+    <div className="flex flex-col w-full h-full justify-start text-text">
+      <div className="h-22 w-full z-50">
+        <Header />
+      </div>
 
-      <div className='z-10 flex jus'>
-
+      <div className="z-10 flex jus">
         {!path ? (
-        <div className='w-[4%] z-50'>
-        <RigthNavigations toCoins={`/overview/${dex ? dex : getActiveButtonDex}/${tokens}`} toPairs={`/overview/${dex ? dex : getActiveButtonDex}/${pairs}`} />
-      </div>
-        ) : null}        
-        <div className={`${!path ? 'w-[96%]' : 'w-[100%]'}  mt-8`}>
-        <Outlet />
+          <div className="w-[4%] z-50">
+            <RigthNavigations
+              toCoins={pathPage.toCoins}
+              toPairs={pathPage.toPairs}
+            />
+          </div>
+        ) : null}
+        <div className={`${!path ? "w-[96%]" : "w-[100%]"}  mt-8`}>
+          <Outlet />
         </div>
       </div>
-      <div>
-        {/* <Footer /> */}
-      </div>
+      <div>{/* <Footer /> */}</div>
     </div>
-  )
-}
+  );
+};
 
-export const Layout = observer(LayoutComponent)
+export const Layout = observer(LayoutComponent);
