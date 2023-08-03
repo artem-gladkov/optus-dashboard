18e77d01d1d121fc15aa2daddba489c3d638cf2f
Alexey Nekrasov <necrasoff.alex@yandex.ru>
2023-08-03 15:37:15 +0000

    fix: bakground

--- src/components/background/bg.tsx
+++ src/components/background/bg.tsx
@@ -9,7 +9,7 @@ const Bg = (props: Props) => {
 
 
   return (
-    <div className='swapanimation  absolute  z-10  bg-bg h-full w-full'>
+    <div className='swapanimation  absolute  z-10  bg-bg h-full w-full -ml-16'>
       <div className='swapbutton h-full w-full z-10 flex justify-center bg-bg '>
           <span className=''></span>
           <span className='-ml-80'></span>
