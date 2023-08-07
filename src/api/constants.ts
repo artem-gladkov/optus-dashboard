dcf5706a0885c65ba219cc37aa3dc91785b132b1
Alexey Nekrasov <necrasoff.alex@yandex.ru>
2023-08-07 07:31:01 +0000

    add: constants

---
+++ src/api/constants.ts
@@ -0,0 +1,12 @@
+export const API_URL = {
+  baseURL: "https://api.dev.optus.fi/api/v1/dashboard/",
+  searchURL: "search?value=",
+  dex_overview_URL: "dex_overview",
+  dex: "dex?dex=",
+  tokens: "token?token_address=",
+  pairs: "pair?pair_id=",
+  account: "account?account_address=",
+  transaction: "transaction?transaction_id=",
+  dex_list: "dex_list?limit=",
+  
+};
